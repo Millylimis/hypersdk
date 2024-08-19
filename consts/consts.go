2207f20462b9c854d13fca07326128812ed91c14
Millylimis <dunamildred@gmail.com>
2024-08-19 23:01:06 +0100

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Limis"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Milly"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "Mill"
 )
 
 var ID ids.ID
